e07248f82e51d30fd98a42de6e7d9e5a32f43ab4
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2023-02-11 22:34:46 +0700

    JWT Auth

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from 'dotenv';
+
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
@@ -13,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = "mongodb+srv://memories:memories123@cluster0.cglxya1.mongodb.net/?retryWrites=true&w=majority"
 
